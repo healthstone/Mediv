ddea243eea56e7d195d42e4dba952bb7a2d136d0
Владимиров Александр <kvipka90@yandex.ru>
2025-01-13 21:21:58 +0300

    added Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,35 @@
+def UUID_DIR  = UUID.randomUUID().toString()
+def url = "https://jenkins.snow1k.com/job/MedivhEmu/${BUILD_NUMBER}/"
+
+pipeline {
+
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                //sh("")
+            }
+        }
+    }
+
+    post {
+        always {
+            script {
+                String emoj = getEmoj(currentBuild.result)
+                rocketSend channel: '#wowemu', message: "MedivhEmu - BUILD-STATUS : ${currentBuild.result}. Report: ${url}", emoji: emoj, rawMessage: true
+            }
+        }
+    }
+
+}
+
+def String getEmoj(String status) {
+    switch(status) {
+        case 'SUCCESS': return ':smirk:'
+        case 'FAILURE': return ':sob:'
+        case 'UNSTABLE': return ':thinking:'
+        default:
+            return ':sob:'
+    }
+}
