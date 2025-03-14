26252aac17ed0e888ce7347ed115ae04f551ef21
Shauren <shauren.trinity@gmail.com>
2025-03-14 19:09:15 +0100

    Core/Misc: Reduce differences between branches

--- src/server/scripts/Battlefield/BattlefieldWG.cpp
+++ src/server/scripts/Battlefield/BattlefieldWG.cpp
@@ -25,6 +25,7 @@
 #include "BattlefieldMgr.h"
 #include "Battleground.h"
 #include "CreatureTextMgr.h"
+#include "DBCStores.h"
 #include "GameTime.h"
 #include "Log.h"
 #include "MapManager.h"
@@ -462,7 +463,7 @@ void BattlefieldWG::OnBattleEnd(bool endByTimer)
 // *******************************************************
 void BattlefieldWG::DoCompleteOrIncrementAchievement(uint32 achievement, Player* player, uint8 /*incrementNumber*/)
 {
-    AchievementEntry const* achievementEntry = sAchievementMgr->GetAchievement(achievement);
+    AchievementEntry const* achievementEntry = sAchievementStore.LookupEntry(achievement);
 
     if (!achievementEntry)
         return;
